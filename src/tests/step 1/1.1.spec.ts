6af6e8b30231be22e1a49d28aaeeef5426547934
Pedro Osternack Correa <pedrost.rj@gmail.com>
2021-08-31 18:35:45 -0400

    migrating to mocha+chai to get ordered tests and proper bail support.

--- src/tests/step 1/1.1.spec.ts
+++ src/tests/step 1/1.1.spec.ts
@@ -1,14 +1,16 @@
 import add from "../../src/calculator";
 import { formatTestName } from "../utils";
+import { expect } from 'chai';
+import 'mocha';
 
 const testCases = [
-    { name: "given a string with a single value should return the same value", input: "42", expected: 42 }
+    { name: "given an empty string should return", input: "", expected: 0 },
 ];
 
 describe("Step 1 - Sum up to 2 comma separated values.", function() {
     testCases.forEach(({name, input, expected}, i) => {
-        test(formatTestName(1, i+1, name, input, expected), () => {
-            expect(add(input)).toEqual(expected);
+        test(formatTestName(1, i, name, input, expected), () => {
+            expect(add(input)).to.equal(expected);
         });
     });
 });
--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,3 +1,3 @@
 export function formatTestName(step: number, testCaseIndex: number, testName: string, input: string, expected: number) {
-    return `Step ${step}.${testCaseIndex + 1} - ${testName}. input: ${input}; expected: ${expected}.`;
+    return `Step ${step}.${testCaseIndex + 1} - ${testName}.\n\tinput: "${input}"; expected: ${expected}`;
 }
