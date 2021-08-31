def78e559e1fbfb163dc761141bd19827649410c
Pedro Osternack Correa <pedrost.rj@gmail.com>
2021-08-31 19:01:33 -0400

    Introducing the ChallengeStep and TestCase interfaces.
    Refactoring the testing code to utilize the new interfaces
    This makes it easier to define multiple testCases for the same Step

--- src/tests/1.spec.ts
+++ src/tests/1.spec.ts
@@ -1,19 +1,22 @@
 import add from "../calculator";
-import { formatTestName } from "./utils";
+import { ChallengeStep, escapeNewLines } from "./utils";
 import { expect } from 'chai';
 import 'mocha';
 
-const testCases = [
-    { name: "given an empty string should return", input: "", expected: 0 },
-    { name: "given a string with a single value should return the same value", input: "42", expected: 42 },
-    { name: "given a string with two comma separated values should return the sum of them", input: "2,2", expected: 4 },
-    { name: "given a string with N comma separated values should return the sum of all of them", input: "1,2,3,4,5", expected: 15 },
+const steps: ChallengeStep[] = [
+    { Title: "given an empty string should return", TestCases: [{Input: "", Expected: 0}]},
+    { Title: "given a string with a single value should return the same value", TestCases: [{Input: "42", Expected: 42}]},
+    { Title: "given a string with two comma separated values should return the sum of them", TestCases: [{Input: "2,2", Expected: 4}]},
+    { Title: "given a string with N comma separated values should return the sum of all of them", TestCases: [{Input: "1,2,3,4,5", Expected: 15}]},
+    { Title: "should accept either commas and/or new lines ('\\n') as value separators.", TestCases: [{Input: "1\n2,3", Expected: 6}]},
 ];
 
-describe("Step 1 - Sum comma separated values.", function() {
-    testCases.forEach(({name, input, expected}, i) => {
-        it(formatTestName(1, i, name, input, expected), () => {
-            expect(add(input)).to.equal(expected);
+steps.forEach((step, stepIndex) => {
+    describe(`Step ${stepIndex} - ${step.Title}`, () => {
+        step.TestCases.forEach((testCase, testCaseIndex) => {
+            it(`input: "${escapeNewLines(testCase.Input)}"; expected: ${testCase.Expected}`, () => {
+                expect(add(testCase.Input)).to.equal(testCase.Expected);
+            });
         });
     });
 });
--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,3 +1,22 @@
-export function formatTestName(step: number, testCaseIndex: number, testName: string, input: string, expected: number) {
-    return `Step ${step}.${testCaseIndex + 1} - ${testName}.\n\tinput: "${input}"; expected: ${expected}`;
+export function formatTestName(
+    step: number,
+    testCaseIndex: number,
+    testName: string,
+    input: string,
+    expected: number) {
+    return `Step ${step}.${testCaseIndex + 1} - ${testName}.\n\tinput: "${escapeNewLines(input)}"; expected: ${expected}`;
+}
+
+export function escapeNewLines(text: string): string {
+    return text.replace('\n', '\\n');
+}
+
+export interface ChallengeStep {
+    Title: string;
+    TestCases: TestCase[];
+}
+
+export interface TestCase {
+    Input: string;
+    Expected: number;
 }
