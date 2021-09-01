5b4b978ed9e581852b93556d9d97ff00b1e56afc
Pedro Osternack Correa <pedrost.rj@gmail.com>
2021-08-31 20:53:08 -0400

    Changing how we assert Errors. Using the try/catch approach we can provide more feedback to the user.

--- src/tests/1.spec.ts
+++ src/tests/1.spec.ts
@@ -8,8 +8,17 @@ steps.forEach((step, stepIndex) => {
     describe(`Step ${stepIndex + 1} - ${step.Title}`, () => {
         step.TestCases.forEach((testCase, testCaseIndex) => {
             if ("Error" in testCase) {
-                it(`Test Case ${testCaseIndex + 1} - input: "${EscapeNewLines(testCase.Input)}"; expected error: "${testCase.Error}"`, () => {
-                    expect(() => add(testCase.Input)).to.Throw((testCase).Error);
+                it(`Test Case ${testCaseIndex + 1} - input: "${EscapeNewLines(testCase.Input)}"; expected error: "${testCase.Error}"`, (done) => {
+                    // using the try/catch approach to assert on errors so we can give more feedback to the dev if necessary
+                    let result;
+                    try {
+                        result = add(testCase.Input);
+                    } catch(error) {
+                        expect(error.message).to.equal(testCase.Error);
+                        done();
+                    }
+                    
+                    expect.fail(`An error was expected, but instead the code returned the value: ${result}`);
                 }); 
             } else {
                 it(`Test Case ${testCaseIndex + 1} - input: "${EscapeNewLines(testCase.Input)}"; expected value: ${testCase.Expected}`, () => {
